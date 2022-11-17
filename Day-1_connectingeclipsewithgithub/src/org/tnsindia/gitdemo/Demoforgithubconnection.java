22dc54d7b8997d52783fcdd2f2046b74a0149ebe
ANU BASAVARAJU <ANU BASAVARAJU@DESKTOP-6VI7UEO>
2022-11-17 15:42:01 +0530

    first commit

---
+++ Day-1_connectingeclipsewithgithub/src/org/tnsindia/gitdemo/Demoforgithubconnection.java
@@ -0,0 +1,10 @@
+package org.tnsindia.gitdemo;
+
+//to connect eclipse with github 
+public class Demoforgithubconnection {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to tns India foundation");
+	}
+
+}
