cd16b8287e6e4057f87ddf425b035cc17233b7cc
ANU BASAVARAJU <ANU BASAVARAJU@DESKTOP-6VI7UEO>
2022-11-17 21:46:34 +0530

    first commit

---
+++ Day-2_datatypes/src/org/tnsindia/demo/ByteCodeDataType.java
@@ -0,0 +1,30 @@
+package org.tnsindia.demo;
+
+public class ByteCodeDataType {
+
+	public static void main(String[] args) {
+		int xyz=125;
+		System.out.println("The value is:"+xyz);
+		long _abc=21478383L;
+		System.out.println("The value is:"+_abc);
+		char num='a';
+		System.out.println("The value is:"+num);
+		float a=15.67f;
+		System.out.println("The value is:"+a);
+		boolean var=true;
+		System.out.println("The value is:"+var);
+		String str="anusha";
+		System.out.println("The value is:"+str);
+		float y=(float)34;
+		System.out.println("The value is:"+y);
+		int i=(int)12.5;
+		System.out.println("The value is:"+i);
+		
+		
+		
+		
+		
+		
+	}
+
+}
