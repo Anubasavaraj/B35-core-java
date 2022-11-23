a7bee7a463249ed9cb04dcb9ae104233574fa39c
ANU BASAVARAJU <ANU BASAVARAJU@DESKTOP-6VI7UEO>
2022-11-23 22:21:07 +0530

    first commit

---
+++ day_04classandconstructors/src/org/tnsindia/classdemo/Car.java
@@ -0,0 +1,21 @@
+package org.tnsindia.classdemo;
+
+public class Car {
+
+
+		Car()
+		{
+			System.out.println("Default Constructor");
+		}
+		
+		
+		//data members
+		int speed;
+		void speed(int speed)
+		{
+			System.out.println("Speed of the car is: "+speed);
+		}
+
+	}
+
+
---
+++ day_04classandconstructors/src/org/tnsindia/constructordemo/ConstructorExecutors.java
@@ -0,0 +1,17 @@
+package org.tnsindia.constructordemo;
+
+public class ConstructorExecutors {
+
+	public static void main(String[] args) {
+		//object creation
+				Employee e=new Employee();
+				Employee e1=new Employee(25000,"Nikita");
+				Employee e2=new Employee(45000,"Chaitali");
+
+				e1.print();
+				e2.print();
+
+
+	}
+
+}
