f0793b16faa91d49002c1ecb9632f7001d2dcdac
ANU BASAVARAJU <ANU BASAVARAJU@DESKTOP-6VI7UEO>
2022-12-22 16:40:15 +0530

    first commit

---
+++ Day19_multithreadingandconcurrency/src/org/tnsindia/multithreadingdemo/ThreadLifeCycleExexcutor.java
@@ -0,0 +1,10 @@
+package org.tnsindia.multithreadingdemo;
+
+public class ThreadLifeCycleExexcutor {
+
+	public static void main(String[] args) {
+		ThreadLifeCycleDemo d=new ThreadLifeCycleDemo();
+		d.start();
+		}
+
+}
